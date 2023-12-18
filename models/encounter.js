11bf1d046f062cc1d697c2ede6facaad29b5ef5d
SamKnyarko <nyarkosamuelkwesi198@gmail.com>
2023-12-18 12:00:13 +0000

    encounter model

---
+++ models/encounter.js
@@ -0,0 +1,10 @@
+// models/encounter.js
+const mongoose = require('mongoose');
+
+const encounterSchema = new mongoose.Schema({
+  patientID: String,
+  dateAndTime: Date,
+  encounterType: String,
+});
+
+module.exports = mongoose.model('Encounter', encounterSchema);
