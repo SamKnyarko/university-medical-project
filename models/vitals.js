2d5ef1d6114602cb9cae8e0dbb4ea8bcaf539f2b
SamKnyarko <nyarkosamuelkwesi198@gmail.com>
2023-12-18 12:24:58 +0000

    final commit

---
+++ models/vitals.js
@@ -0,0 +1,12 @@
+// models/vitals.js
+const mongoose = require('mongoose');
+
+const vitalsSchema = new mongoose.Schema({
+  patientID: String,
+  bloodPressure: String,
+  temperature: String,
+  pulse: String,
+  spO2: String,
+});
+
+module.exports = mongoose.model('Vitals', vitalsSchema);
