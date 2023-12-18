2675bc116d788400fcbcf59b0df9747567804e9e
SamKnyarko <nyarkosamuelkwesi198@gmail.com>
2023-12-18 11:55:15 +0000

    patients model

---
+++ models/patient.js
@@ -0,0 +1,16 @@
+// models/patient.js
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+  patientID: String,
+  surname: String,
+  otherNames: String,
+  gender: String,
+  phoneNumber: String,
+  residentialAddress: String,
+  emergencyName: String,
+  emergencyContact: String,
+  relationshipWithPatient: String,
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
